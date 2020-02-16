98cf4a52742205823bb14d14e4ad16c2dd362663
Windy <19060@qq.com>
2020-02-16 14:04:45 +0800

    test in iOS

--- CoreI/Status.go
+++ CoreI/Status.go
@@ -25,9 +25,9 @@ func (v *Status) GetApp(name string) string {
 
 func (v *Status) GetTun2socksArgs(localDNS bool, enableIPv6 bool) (ret []string) {
 	ret = []string{"--netif-ipaddr",
-		"26.26.26.2",
+		"26.26.26.4",
 		"--netif-netmask",
-		"255.255.255.252",
+		"255.255.255.0",
 		"--socks-server-addr",
 		"127.0.0.1:10808",
 		"--tunmtu",
@@ -36,11 +36,11 @@ func (v *Status) GetTun2socksArgs(localDNS bool, enableIPv6 bool) (ret []string)
 		"notice",
 		"--enable-udprelay",
 		"--sock-path",
-		v.GetDataDir() + "sock_path",
+		v.GetDataDir() + "sock_path", // android vpn 建立起来之后返回的fd, 用来获取数据
 	}
 
 	if enableIPv6 {
-		ret = append(ret, "--netif-ip6addr", "da26:2626::2")
+		ret = append(ret, "--netif-ip6addr", "da26:2626::4")
 	}
 
 	if localDNS {
@@ -51,13 +51,13 @@ func (v *Status) GetTun2socksArgs(localDNS bool, enableIPv6 bool) (ret []string)
 }
 
 func (v *Status) GetVPNSetupArg(localDNS bool, enableIPv6 bool) (ret string) {
-	ret = "m,1500 a,26.26.26.1,30 r,0.0.0.0,0"
+	ret = "m,1500 a,26.26.26.1,24 r,0.0.0.0,0"
 
 	if enableIPv6 {
-		ret += " a,da26:2626::1,126 r,::,0"
+		ret += " a,da26:2626::1,120 r,::,0" // 这里 da26:2626::1/120 = 255 ips
 	}
 	if localDNS {
-		ret += " d,26.26.26.2"
+		ret += " d,26.26.26.4"
 	}
 	return
 }
