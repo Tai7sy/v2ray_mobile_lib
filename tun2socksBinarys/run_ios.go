721ad6c0911d3a4d47842b11652b1550a16a592e
Tai7sy-Mac <19060@qq.com>
2020-03-04 23:59:33 +0800

    optimize memory for ios

--- tun2socksBinarys/run_ios.go
+++ tun2socksBinarys/run_ios.go
@@ -3,10 +3,6 @@
 
 package tun2socksBinarys
 
-import (
-	"runtime/debug"
-)
-
 type Tun2SocksRun struct {
 	Status interface{}
 }
@@ -17,9 +13,6 @@ func (v *Tun2SocksRun) CheckAndExport() error {
 
 func (v *Tun2SocksRun) Run(sendFd func() int) error {
 	// ios 无法创建子进程, 使用内置的tun2socks
-
-	// ios 内存限制很严格, 加快gc速度
-	debug.SetGCPercent(10)
 	return nil
 }
 
